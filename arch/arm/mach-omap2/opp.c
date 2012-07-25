2d6a5a0488e26af7c72bd42b3a1d9bf2cc7729cb
Ezekeel <notezekeel@googlemail.com>
2012-07-25 23:53:50 +0200

    Added kernel config options 'OMAP_SMARTREFLEX_CUSTOM_SENSOR', 'CONFIG_OMAP_OCFREQ_1400', 'CONFIG_OMAP_OCFREQ_1600', 'CONFIG_OMAP_OCFREQ_1800' and 'CONFIG_OMAP_OCFREQ_2000'.
    Conflicts:
    	arch/arm/mach-omap2/omap2plus-cpufreq.c
    	arch/arm/mach-omap2/opp4xxx_data.c

--- arch/arm/mach-omap2/opp.c
+++ arch/arm/mach-omap2/opp.c
@@ -119,9 +119,11 @@ int __init omap_init_opp_table(struct omap_opp_def *opp_def,
 			if (r)
 				dev_err(dev, "%s:%s:err dvfs register %d %d\n",
 					__func__, opp_def->hwmod_name, r, i);
+
 #ifdef CONFIG_CUSTOM_VOLTAGE
 			customvoltage_register_oppdevice(dev, opp_def->hwmod_name);
 #endif
+
 #ifdef CONFIG_LIVE_OC
 			liveoc_register_oppdevice(dev, opp_def->hwmod_name);
 #endif
