990a685c98571326e0ffc803d644847525c9cc4a
anarkia1976 <stefano.villa1976@gmail.com>
2013-09-03 11:45:31 +0200

    Kernel Version: AK.x802.SKL.SUPERNOVA

--- ak-build.sh
+++ ak-build.sh
@@ -46,7 +46,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="AK.x801.SKL.SUPERNOVA"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="AK.x802.SKL.SUPERNOVA"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
