63cb562e21f0b0c10ace50a8f8554979c9e611d7
Amperific <amperific@gmail.com>
2013-10-12 21:33:26 +0800

    Linux 3.0.99
    Signed-off-by: Amperific <amperific@gmail.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -1646,6 +1646,7 @@ int skb_splice_bits(struct sk_buff *skb, unsigned int offset,
 	struct splice_pipe_desc spd = {
 		.pages = pages,
 		.partial = partial,
+		.nr_pages_max = MAX_SKB_FRAGS,
 		.flags = flags,
 		.ops = &sock_pipe_buf_ops,
 		.spd_release = sock_spd_release,
@@ -1692,7 +1693,7 @@ done:
 		lock_sock(sk);
 	}
 
-	splice_shrink_spd(pipe, &spd);
+	splice_shrink_spd(&spd);
 	return ret;
 }
 
